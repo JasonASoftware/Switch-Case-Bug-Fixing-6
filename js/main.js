afacc779c3f1767b2f2eaeedbe2187b0e63d36b4
JasonASoftware <jason.software123@gmail.com>
2022-10-04 20:22:37 -0400

    added new code to  main js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function evalObject(value){
+    switch(value.operation){
+      case'+': return value.a + value.b;
+        break;
+      case'-': return value.a - value.b;
+        break;
+      case'/': return value.a / value.b;
+        break;
+      case'*': return value.a * value.b;
+        break;
+      case'%': return value.a % value.b;
+        break;
+      case'^': return Math.pow(value.a, value.b);
+    }
+  }
